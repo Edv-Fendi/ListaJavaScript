b81f38500b467da8e8e63575ee2f687686d0efaa
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-09-08 09:37:12 -0300

    01

---
+++ Lista01-WEB/Lista01/Exercicio1/01.js
@@ -0,0 +1,18 @@
+x = prompt('Digite o tamanho no eixo X')
+y = prompt('Digite o tamanho no eixo Y')
+
+area = x * y
+console.log(area)
+
+alert('Seu terreno possui ' + area + 'm')
+
+/*let timeout
+
+function myFunction() {
+  timeout = setTimeout(alertFunc, 3000)
+}
+
+function alertFunc() {
+  alert('Hello!')
+}
+*/
